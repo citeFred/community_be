9a23f7b5b617864f9a4de5dff01dded42274ce02
citeFred <citefred@gmail.com>
2026-02-09 14:36:41 +0900

    feat: 게시글과 파일 1:N 연관관계 설정 및 첨부파일 업로드 기능 구현

--- src/main/java/com/meta/community_be/file/domain/File.java
+++ src/main/java/com/meta/community_be/file/domain/File.java
@@ -1,5 +1,6 @@
 package com.meta.community_be.file.domain;
 
+import com.meta.community_be.article.domain.Article;
 import com.meta.community_be.common.domain.TimeStamped;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -16,12 +17,23 @@ public class File extends TimeStamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column()
+    @Column(nullable = false)
     private String originalFileName;
 
-    @Column()
+    @Column(nullable = false)
     private String storedFileName;
 
-    @Column()
+    @Column(nullable = false)
     private String filePath;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id", nullable = false)
+    private Article article;
+
+    public File(String originalFileName, String storedFileName, String filePath, Article article) {
+        this.originalFileName = originalFileName;
+        this.storedFileName = storedFileName;
+        this.filePath = filePath;
+        this.article = article;
+    }
 }
